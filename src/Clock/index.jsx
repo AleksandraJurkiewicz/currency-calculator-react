ccc035e4b23be65267b9d0a19120d105185759c6
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2025-03-06 18:09:36 +0100

    useCurrentDate added

--- src/Clock/index.jsx
+++ src/Clock/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 import { DateFormat } from "./styled";
 
 const formatDate = (date) => date.toLocaleString(undefined, {
@@ -11,20 +11,12 @@ const formatDate = (date) => date.toLocaleString(undefined, {
 });
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
-
+    const date = useCurrentDate();
+    
     return (
         <DateFormat>
+            Dzisiaj jest 
+            {" "}
             {formatDate(date)}
         </DateFormat>
     )
